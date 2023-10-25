9e19f0662f9d6f9710a15b2b8d16230269650ac8
Konain1 <kaunainkasab@gmail.com>
2023-10-26 00:32:47 +0530

    event capturing

--- deligation.js
+++ deligation.js
@@ -1,10 +1,36 @@
 
 const container = document.querySelector('.container')
+const div = document.querySelector('.div')
+const form = document.querySelector('.form')
+const btn = document.querySelector('.button')
 
+console.log(btn)
 
-container.addEventListener('click',(e)=>{
+// container.addEventListener('click',(e)=>{
 
-    if(e.target.tagName== 'BUTTON'){
-        console.log(e.target.innerText)
-    }
+//     if(e.target.tagName== 'BUTTON'){
+//         console.log(e.target.innerText)
+//     }
+// })
+
+div.addEventListener('click',()=>{
+    console.log('div')
+    alert('div')
+},
+{
+    capture:true
+})
+form.addEventListener('click',(e)=>{
+    // e.preventDefault()
+    console.log('form')
+    alert('form')
+},{
+    capture:true
+})
+btn.addEventListener('click',()=>{
+    console.log('button')
+    alert('button')
+},
+{
+    capture:true
 })
